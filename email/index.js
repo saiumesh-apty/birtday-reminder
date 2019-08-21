42fd13c3788a18daa18e25161fddc8ebcd004e46
saiumesh-apty <saiumesh.dhanewar@apty.io>
2019-08-21 15:34:12 +0530

    changed password 🤷‍♀️🤷‍♀️

--- email/index.js
+++ email/index.js
@@ -19,12 +19,12 @@ async function main() {
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
+        host: 'smtp.ethereal.email',
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: "d.sai535@gmail.com", // generated ethereal user
-            pass: "ChernobylBhainsa@535" // generated ethereal password
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass // generated ethereal password
         }
     });
 
